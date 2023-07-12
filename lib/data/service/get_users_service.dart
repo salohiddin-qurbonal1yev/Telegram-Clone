49d6ee04e779da2bfd57fe9b1bec9e93ba7256de
salohiddin-65 <salohiddink65@gmail.com>
2023-07-12 22:01:41 +0500

    Image upload success fully added👍👍👍👍👍👍

--- lib/data/service/get_users_service.dart
+++ lib/data/service/get_users_service.dart
@@ -1,23 +1,23 @@
-import 'package:dio/dio.dart';
-import 'package:unired_telegram/core/config/dio_catch_error_config.dart';
-import 'package:unired_telegram/core/config/dio_config.dart';
-import 'package:unired_telegram/core/constant/project_url.dart';
-import 'package:unired_telegram/data/model/users_model.dart';
+// import 'package:dio/dio.dart';
+// import 'package:unired_telegram/core/config/dio_catch_error_config.dart';
+// import 'package:unired_telegram/core/config/dio_config.dart';
+// import 'package:unired_telegram/core/constant/project_url.dart';
+// import 'package:unired_telegram/data/model/users_model.dart';
 
-class UsersService {
-  Future<dynamic> getUsers() async {
-    try {
-      Response response =
-          await DioConfig.createRequest().get(TelegramUrls.users);
-      if (response.statusCode == 200) {
-        return (response.data as List)
-            .map((e) => UsersModel.fromJson(e))
-            .toList();
-      } else {
-        return response.statusMessage.toString();
-      }
-    } on DioException catch (e) {
-      return DioCatchErrorConfig.catchError(e);
-    }
-  }
-}
+// class UsersService {
+//   Future<dynamic> getUsers() async {
+//     try {
+//       Response response =
+//           await DioConfig.createRequest().get(TelegramUrls.users);
+//       if (response.statusCode == 200) {
+//         return (response.data as List)
+//             .map((e) => UsersModel.fromJson(e))
+//             .toList();
+//       } else {
+//         return response.statusMessage.toString();
+//       }
+//     } on DioException catch (e) {
+//       return DioCatchErrorConfig.catchError(e);
+//     }
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:unired_telegram/core/router/router.dart';
 import 'package:unired_telegram/provider/auth/auth_provider.dart';
 import 'package:unired_telegram/provider/language_provider.dart';
 import 'package:unired_telegram/provider/otp_provider.dart';
-import 'package:unired_telegram/view/home/home_page.dart';
 
 void main() async {
   // Initialize widgets flutter binding
@@ -50,9 +50,9 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       debugShowCheckedModeBanner: false,
-      // onGenerateRoute: RouteGenerator.router.onGenerate,
-      // initialRoute: 'start',
-      home: const HomePage(),
+      onGenerateRoute: RouteGenerator.router.onGenerate,
+      initialRoute: 'start',
+      // home: const HomePage(),
     );
   }
 }
